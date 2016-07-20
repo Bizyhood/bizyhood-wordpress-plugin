82adbbbf4eb47e0dff9a8ecc23314a81ee0eed42
Spyros Vlachopoulos <ludwigvb@gmail.com>
2016-07-20 09:35:43 +0300

    #comment BP-10 Take out the search bar from business-directory and replace it with our widget #resolve

--- Bizyhood/Views/listings/index.php
+++ Bizyhood/Views/listings/index.php
@@ -1,19 +1,6 @@
 <div class="row bh_business-header">
-    <div class="col-md-8 bh_business-header-title">	
+    <div class="col-md-12 bh_business-header-title">	
         <h3>Businesses</h3>
-    </div>
-    <div class="col-md-4 bh_business-search">
-    <?php
-      $keywords = stripslashes($keywords);
-    ?>
-    <form action="<?php echo site_url(); ?>/index.php" method="get">
-        <input type="hidden" name="page_id" value="<?php echo $list_page_id; ?>">
-        <?php if (isset($_GET['cf'])) { ?>
-        <input type="hidden" name="cf" value="<?php echo (urldecode($_GET['cf'])); ?>">
-        <?php } ?>
-        <input type="search" class="bh_search-field" placeholder="Search businesses..." name="keywords" title="Search for:" value="<?php echo $keywords; ?>">
-        <button class="search_submit" ><span class="entypo-search"></span></button>
-    </form>
     </div>
 </div>
 <div class="row">			
