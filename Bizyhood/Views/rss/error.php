6d1671ea790940e85e97100e7c9eea9036863a78
Spyros Vlachopoulos <ludwigvb@gmail.com>
2016-11-12 17:20:50 +0200

    BP-43 adding rss/error.php

---
+++ Bizyhood/Views/rss/error.php
@@ -0,0 +1,24 @@
+<?php
+  header('Content-Type: '.feed_content_type('rss-http').'; charset='.get_option('blog_charset'), true);
+  echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+?>
+<rss version="2.0"
+        xmlns:content="http://purl.org/rss/1.0/modules/content/"
+        xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:atom="http://www.w3.org/2005/Atom"
+        xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+        xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+        <?php do_action('rss2_ns'); ?>>
+<channel>
+        <title><?php bloginfo_rss('name'); ?> - Feed</title>
+        <atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
+        <link><?php bloginfo_rss('url') ?></link>
+        <description><?php bloginfo_rss('description') ?></description>
+        <lastBuildDate><?php echo date('c'); ?></lastBuildDate>
+        <language><?php echo get_option('rss_language'); ?></language>
+        <sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'daily' ); ?></sy:updatePeriod>
+        <sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+        <?php do_action('rss2_head'); ?>
+</channel>
+</rss>
